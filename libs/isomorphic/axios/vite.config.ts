45b01d01c31f54d31f85d41065716e37710d20d4
icedlee337 <lee.e.norris@gmail.com>
2025-06-29 21:40:06 -0500

    chore: remove onix

--- libs/isomorphic/axios/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { viteConfigFactory } from '@onivoro/onix';
-
-export default viteConfigFactory({
-    root: __dirname,
-    tsconfigPath: 'tsconfig.lib.json',
-    cacheDir: '../../../node_modules/.vite/libs/isomorphic/axios',
-    outDir: '../../../dist/libs/isomorphic/axios',
-});
