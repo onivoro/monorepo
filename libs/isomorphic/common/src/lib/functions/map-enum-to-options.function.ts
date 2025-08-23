549c9b97d518d031d4ca2527e112bf111fde3f40
icedlee337 <lee.e.norris@gmail.com>
2025-08-23 14:32:14 -0500

    fix: missing import

--- libs/isomorphic/common/src/lib/functions/map-enum-to-options.function.ts
+++ libs/isomorphic/common/src/lib/functions/map-enum-to-options.function.ts
@@ -1,7 +1,9 @@
+import { ILookup } from "../types/lookup.interface";
+
 export function mapEnumToOptions<TEntity extends object>(enumeration: TEntity, includeBlank = true): ILookup<string, string> {
     const enumArray = Object.entries(enumeration).map(([key, value]) => ({ value, display: key.replace(/_/g, ' ') }))
 
-    return includeBlank
+    return (includeBlank
         ? [{ display: '', value: '' }, ...enumArray]
-        : enumArray;
+        : enumArray) as any;
 }
